88fe890b9ae59b23b8236f3d8d028cb49aba8fa6
yaniv preiss <yanivpr@gmail.com>
2014-07-30 20:21:37 +0300

    added specs

--- spec/phrase_file_spec.rb
+++ spec/phrase_file_spec.rb
@@ -4,6 +4,7 @@ describe PhraseFile do
 
   before(:each) do
     subject { PhraseFile.new }
+    file_content! default_content
   end
 
   context "random" do
@@ -19,6 +20,32 @@ describe PhraseFile do
 
       subject.random.should eq 'phrase three'
     end
-
   end
+
+  context "append" do
+    it "should append phrase to end of file" do
+      new_phrase = "phrase four"
+
+      subject.append new_phrase
+
+      read_file_content.should eq "#{default_content} | #{new_phrase}"
+    end
+  end
+
+end
+
+def file_content!(content)
+  spec_source_file = "#{Rails.root}/spec/phrases_source.txt"
+
+  File.open(spec_source_file, 'w+') { |f| f.write(content) }
+end
+
+def read_file_content
+  spec_source_file = "#{Rails.root}/spec/phrases_source.txt"
+
+  File.read(spec_source_file)
+end
+
+def default_content
+  "phrase one | phrase two | phrase three"
 end
